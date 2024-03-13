85f00966220116457925242a7d9819cf3403ca05
Bryan Julio <bryan.19julio@gmail.com>
2024-03-12 22:36:12 -0300

    #1 commit

---
+++ api/controller/robot_ob.py
@@ -0,0 +1,6 @@
+def config_robo():
+    config = {'LOGIN': {'email': 'bryan.19julio@gmail.com', 'senha': '7kcYp3PB@Z*Y!v$'}, 'AJUSTES': {'valor_entrada': '5', 'tipo': 'automatico', 'stop_win': '10000', 'stop_loss': '10000', 'analise_medias': 'N', 'velas_medias': '10'}, 'MARTINGALE': {'usar_martingale': 'S', 'niveis_martingale': '1', 'fator_martingale': '1'}, 'SOROS': {'usar_soros': 'S', 'niveis_soros': '1'}}
+    return config
+
+def teste():
+    return '1'
